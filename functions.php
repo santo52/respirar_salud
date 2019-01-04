44229f429cd7aefdeef88f9bc0eeb7d545219392
Santiago Ruiz <sanruiz1003@gmail.com>
2019-01-04 17:57:46 -0500

    https to lib

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if(!function_exists('respirar_salud_scripts')):
         wp_register_style( 'main', get_template_directory_uri() . '/css/main.css', ['normalize'], $version, 'all' );
         wp_register_style( 'style', get_stylesheet_uri(), ['main'], $version, 'all' );
         wp_register_script( 'jQuery_js', "https://ajax.googleapis.com/ajax/libs/jquery/{$jQueryVersion}/jquery.min.js", array( 'jquery' ), $jQueryVersion, true );
-        wp_register_script( 'tween_max', "http://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js", array( 'jQuery_js' ), $version, true );
+        wp_register_script( 'tween_max', "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js", array( 'jQuery_js' ), $version, true );
         wp_register_script( 'script', get_template_directory_uri() . '/js/script.js', array('tween_max'), $version , true );
 
 
