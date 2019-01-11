7e509154decfb1e47217789075c3ffa912dcbca7
Santiago Ruiz <sanruiz1003@gmail.com>
2019-01-10 20:10:19 -0500

    finalización de slider

--- content.php
+++ content.php
@@ -11,8 +11,8 @@
         </ul>
     </nav>
 </header>
-<div class="Slide-Container">
-    <span class="Icon"><img src="<?php echo get_template_directory_uri() . '/images/icons/arrow-left.png' ?>" ></span>
+<div class="Slide-Container" data-time="4000" >
+    <span class="Icon left"><img src="<?php echo get_template_directory_uri() . '/images/icons/arrow-left.png' ?>" ></span>-->
     <div class="Slide">
         <div class="Slide-Item">
             <img src="<?php echo get_template_directory_uri() . '/images/background.jpg' ?>"  alt="background" class="Slide-Item-Background gray-scale">
@@ -21,8 +21,29 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
             </span>
         </div>
+        <div class="Slide-Item">
+            <img src="<?php echo get_template_directory_uri() . '/images/team11.jpg' ?>"  alt="background2" class="Slide-Item-Background gray-scale">
+            <span class="Slide-Item-Complement Leyend">
+                <h2>Consulta Médica Domiciliaria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+            </span>
+        </div>
+        <div class="Slide-Item">
+            <img src="<?php echo get_template_directory_uri() . '/images/portafolio_1.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
+            <span class="Slide-Item-Complement Leyend">
+                <h2>Consulta Médica Domiciliaria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+            </span>
+        </div>
+        <div class="Slide-Item">
+            <img src="<?php echo get_template_directory_uri() . '/images/Portafolio_8.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
+            <span class="Slide-Item-Complement Leyend">
+                <h2>Consulta Médica Domiciliaria</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+            </span>
+        </div>
     </div>
-    <span class="Icon"><img src="<?php echo get_template_directory_uri() . '/images/icons/arrow-right.png' ?>" ></span>
+    <span class="Icon right"><img src="<?php echo get_template_directory_uri() . '/images/icons/arrow-right.png' ?>" ></span>
 </div>
 
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 ;
+
+var intervalSliders;
+
+$(document).ready(function(e){
+	showSliders();
+	intervalSliders = automoveSliders();
+});
+
+
+
+
+
+
+
+
+
+
+//Funcions 
+
 $=jQuery.noConflict();
-console.log('loadparallax');
 $.fn.parallax = function ( resistance, mouse ) 
 {
 	$el = $( this );
@@ -12,12 +30,78 @@ $.fn.parallax = function ( resistance, mouse )
 };
 
 
+function showSliders(){
+	let $slide = $('.Slide-Container');
+	$slide.each(function(i, elem){
+		let $thisSlide = $(elem).data('id-container', i).children('.Slide');
+		let $thisSlideItem = $thisSlide.children('.Slide-Item');
+		let count = $thisSlideItem.length;
+		if(count == 0){
+			$slide.eq(i).remove();
+		} else {
+			$thisSlideItem
+			.css('width', (100 / count) + '%')
+			.eq(0).addClass('active');
+
+			$thisSlide	
+			.css('width', (count * 100) + '%')
+			.data('items', count)
+			.data('position', 1);
+		}
+	});
+}
+
+function automoveSliders(){
+	var $sliders = $('.Slide-Container');
+	return $sliders.map(function(i, elem){
+		let milliseconds = $sliders.eq(i).data('time') || 4000;
+		clearInterval(intervalSliders);
+		return setInterval(function(){
+			moveSlider($sliders.eq(i).find('.right').eq(0));
+		}, milliseconds);
+	});
+}
+
+//Events
+
 $( document ).mousemove( function( e ) {
     $( '.Slide-Item-Background' ).parallax( -50, e );
-	
 	let $complement = $( '.Slide-Item .Slide-Item-Complement' );
 	$.each($complement, function(i, element){
 		var value = (i + 1) * 150;
 		$(element).parallax( value, e );
 	});
+});
+
+
+function moveSlider($this){
+	
+	let count = $this.siblings('.Slide').data('items');
+	let position = $this.siblings('.Slide').data('position');
+	let idContainer = $this.parent().data('id-container');
+
+	if($this.hasClass('left')){
+		position--;
+		if(position == 0){
+			position = count;
+		}
+	} else {
+		position++;
+		if(position > count){
+			position = 1;
+		}
+	}
+
+	let currentPosition = (position-1);
+	$this.siblings('.Slide')
+	.data('position', position)
+	.css('left', '-' + (currentPosition*100) + '%')
+	.children('.Slide-Item')
+	.removeClass('active')
+	.eq(currentPosition).addClass('active');
+}
+
+
+$('.Slide-Container .Icon').on('click', function(e){
+	moveSlider($(this));
 });
