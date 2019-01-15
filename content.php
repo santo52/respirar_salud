3dcfbe95ea675cbe26c5463d7f3ce795bc8ebb23
Santiago Ruiz <sanruiz1003@gmail.com>
2019-01-15 04:58:29 -0500

    correcciones

--- content.php
+++ content.php
@@ -1,35 +1,35 @@
 <input type="hidden" id="uri_json_temp" value="<?php echo get_template_directory_uri() . '/js/descripciones.json'; ?>">
-<div class="Slide-Container" data-time="4000" >
+<div class="Slide-Container" data-time="10000" >
     <span class="Icon left"><img src="<?php echo get_template_directory_uri() . '/images/icons/arrow-left.png' ?>" ></span>-->
     <div class="Slide">
         <div class="Slide-Item">
-            <img src="<?php echo get_template_directory_uri() . '/images/bubbles.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
+            <img src="<?php echo get_template_directory_uri() . '/images/home_inicial.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
             <span class="Slide-Item-Complement" id="logo-header">
-                <img src="<?php echo get_template_directory_uri() . '/images/logo.svg' ?>" alt="medalla">
+                <img src="<?php echo get_template_directory_uri() . '/images/logo_blanco.svg' ?>" alt="medalla">
             </span>
             <span class="Slide-Item-Complement" id="medalla">
-                <img src="<?php echo get_template_directory_uri() . '/images/medalla.svg' ?>" alt="medalla">
+                <img src="<?php echo get_template_directory_uri() . '/images/medalla.png' ?>" alt="medalla">
             </span>
         </div>
         <div class="Slide-Item">
             <img src="<?php echo get_template_directory_uri() . '/images/team11.jpg' ?>"  alt="background2" class="Slide-Item-Background gray-scale">
             <span class="Slide-Item-Complement Leyend">
-                <h2>Consulta Médica Domiciliaria</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+                <h2>ATENCIÓN MÉDICA DOMICILIARÍA:</h2>
+                <p>Nuestro servicio de consulta médica prioritaria tiene el objetivo de la evaluación de los síntomas específicos, definir la posibilidad de atención en domicilio o generar direccionamiento de traslado a una IPS hospitalaria, según análisis de síntomas evidenciados.  </p>
             </span>
         </div>
         <div class="Slide-Item">
             <img src="<?php echo get_template_directory_uri() . '/images/portafolio_1.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
             <span class="Slide-Item-Complement Leyend">
-                <h2>Slider 3</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+                <h2>SALUD KIDS</h2>
+                <p>Para las atenciones de todos los niños, niñas y adolescentes hasta los 14 años, Respirar salud, cuenta con atención médica domiciliaria, por medio de profesionales especializados en pediatría, quienes, valoran, diagnostican y realizan seguimientos, desde la comodidad de su hogar.</p>
             </span>
         </div>
         <div class="Slide-Item">
             <img src="<?php echo get_template_directory_uri() . '/images/Portafolio_8.png' ?>"  alt="background" class="Slide-Item-Background gray-scale">
             <span class="Slide-Item-Complement Leyend">
-                <h2>Slider 4</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro asperiores molestiae beatae harum non nostrum, sunt voluptatum iure eum deleniti aspernatur ab voluptates! Iure quae sed dignissimos numquam doloribus.  </p>
+                <h2>ENFERMERÍA</h2>
+                <p>Nuestro personal de enfermería está capacitado para manejo de actividades básicas como asistencia alimentaria, cuidados y aseo de la piel, procedimientos menores, cambios de sonda, cateterismos intermitentes, curaciones de baja, mediana y alta complejidad, aplicación de medicamentos y amplia experiencia en pacientes en condición aguda y crónica a nivel Domiciliaria.</p>
             </span>
         </div>
     </div>
@@ -50,7 +50,7 @@
 <section class="Services Over" style="background-image: url( <?php echo get_template_directory_uri() . '/images/nuestros_servicios.png' ?> )">
     <section class="Description hide">
         <article>
-            <h3 class="text-green">prueba</h2>
+            <h3 class="text-green">titulo</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempore pariatur nemo, praesentium soluta fugit sit quas recusandae nobis, maxime molestiae laborum asperiores dicta, iure ullam perferendis. Accusamus, qui quos!</p>
         </article>
     </section>
@@ -70,46 +70,49 @@
         </nav>
     </section>
 </section>
-<section class="News Over" style="background-image: url( <?php echo get_template_directory_uri() . '/images/background.jpg' ?> )" >
+<section class="News Over" style="background-image: url( <?php echo get_template_directory_uri() . '/images/notas_de_interes.jpg' ?> )" >
     <h2 class="text-white">notas de interés</h2>
     <p  class="text-white">Enterate de nuestras últimas noticias e innovaciones</p>
     <section class="Last-News">
         <div class="News-Container">
         <article class="New Flip-Card">
-                <div class="Card">
+                <a class="Card">
                     <div class="Card-Face Front"><img src="<?php echo get_template_directory_uri() . '/images/noti1.png' ?>" alt=""></div>
                     <div class="Card-Face Back">
                         <div class="Card-Text">
                             <span class="date">01/01/2019</span>
                             <h4 class="text-green">Servicios de terapias</h4>
-                            <p>Lorem, ipsum dolor sit amet consectetur !</p>
+                            <p>Las bajas temperaturas, cambios climaticos bruscos y la contaminación ambiental son factores que contribuyen a la aparición de la gripe. </p>
+                            <span class="link">Terapias físicas y respiratorias</span>
                         </div>
                     </div>
-                </div>
+                </a>
             </article>
             <article class="New Flip-Card">
-                <div class="Card">
+            <a class="Card">
                     <div class="Card-Face Front"><img src="<?php echo get_template_directory_uri() . '/images/noti2.png' ?>" alt=""></div>
                     <div class="Card-Face Back">
                         <div class="Card-Text">
                             <span class="date">01/01/2019</span>
                             <h4 class="text-green">Servicios de terapias</h4>
-                            <p>Lorem, ipsum dolor sit amet consectetur !</p>
+                            <p>Las bajas temperaturas, cambios climaticos bruscos y la contaminación ambiental son factores que contribuyen a la aparición de la gripe. </p>
+                            <span class="link">Terapias físicas y respiratorias</span>
                         </div>
                     </div>
-                </div>
+                </a>
             </article>
             <article class="New Flip-Card">
-                <div class="Card">
+            <a class="Card">
                     <div class="Card-Face Front"><img src="<?php echo get_template_directory_uri() . '/images/noti3.png' ?>" alt=""></div>
                     <div class="Card-Face Back">
                         <div class="Card-Text">
                             <span class="date">01/01/2019</span>
                             <h4 class="text-green">Servicios de terapias</h4>
-                            <p>Lorem, ipsum dolor sit amet consectetur !</p>
+                            <p>Las bajas temperaturas, cambios climaticos bruscos y la contaminación ambiental son factores que contribuyen a la aparición de la gripe. </p>
+                            <span class="link">Terapias físicas y respiratorias</span>
                         </div>
                     </div>
-                </div>
+                </a>
             </article>
         </div>
     </section>
