6d1ab4cc6039ed8a430296a633d7d9d0fcb06876
Santiago Ruiz <sanruiz1003@gmail.com>
2019-01-29 03:23:01 -0500

    implementación de owl carousel en single.php

--- functions.php
+++ functions.php
@@ -9,10 +9,14 @@ if(!function_exists('respirar_salud_styles')):
         wp_register_style( 'fonts', 'https://fonts.googleapis.com/css?family=Mukta:700|Open+Sans:300,300i,400' );
         wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.min.css', [], $version, 'all' );
         wp_register_style( 'muicss', '//cdn.muicss.com/mui-0.9.41/css/mui.min.css', ['normalize'],  $version, 'all');
-        wp_register_style( 'main', get_template_directory_uri() . '/css/main.css', ['muicss'], $version, 'all' );
+        wp_register_style( 'owl_css', get_template_directory_uri() . '/css/assets/owl.carousel.min.css', ['normalize'],  $version, 'all');
+        wp_register_style( 'owl_theme', get_template_directory_uri() . '/css/assets/owl.theme.default.min.css', ['owl_css'],  $version, 'all');
+        wp_register_style( 'main', get_template_directory_uri() . '/css/main.css', ['muicss', 'owl_theme'], $version, 'all' );
         wp_register_style( 'style', get_stylesheet_uri(), ['main'], $version, 'all' );
         
         wp_enqueue_style( 'muicss' );
+        wp_enqueue_style( 'owl_css' );
+        wp_enqueue_style( 'owl_theme' );
         wp_enqueue_style( 'main' );
         wp_enqueue_style( 'style' );
         wp_enqueue_style( 'fonts' );
@@ -33,12 +37,14 @@ if(!function_exists('respirar_salud_scripts')):
 
         wp_register_script( 'jQuery_js', "https://ajax.googleapis.com/ajax/libs/jquery/{$jQueryVersion}/jquery.min.js", array( 'jquery' ), $jQueryVersion, true );
         wp_register_script( 'tween_max', "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js", array( 'jQuery_js' ), $version, true );
+        wp_register_script( 'owl_js', get_template_directory_uri() . '/js/owl.carousel.min.js', array( 'jQuery_js' ), $version, true );
         wp_register_script( 'muijs', "//cdn.muicss.com/mui-0.9.41/js/mui.min.js", array(), $version, true );
         wp_register_script( 'functions', get_template_directory_uri() . '/js/functions.min.js', array('tween_max', 'muijs'), $version , true );
         wp_register_script( 'script', get_template_directory_uri() . '/js/script.min.js', array('functions'), $version , true );
 
 
         wp_enqueue_script( 'muijs' );
+        wp_enqueue_script( 'owl_js' );
         wp_enqueue_script( 'jQuery_js' );
         wp_enqueue_script( 'tween_max' );
         wp_enqueue_script( 'functions' );
@@ -74,3 +80,71 @@ if(!function_exists('respirar_salud_menus')) :
 endif;
 
 add_action('init', 'respirar_salud_menus');
+
+
+
+
+  /*
+ function dc_related_after_content( $content ) 
+ { 
+    
+    if ( !is_singular('post') ) return $content;	
+	
+	$cad			= "";
+	$template_li 	= '<li>
+							<a class="thumb_rel" href="{url}">{thumb}</a>
+							<a class="title_rel" href="{url}">{title}</a>
+						</li>';
+	$template_rel	= '<div class="rel_posts">
+							<h3>Artículos Relacionados</h3>
+							<ul>
+								{list}
+							</ul>
+					   </div>';
+
+    $terms = get_the_terms( get_the_ID(), 'category');
+    $categ = array();   
+    
+    if ( $terms )
+    {
+    	foreach ($terms as $term) 
+    	{
+    		$categ[] = $term->term_id;
+    	}
+    }
+    else{
+    	return $content;
+    }
+
+   $loop	= new WP_QUERY(array(
+    				'category__in'		=> $categ,
+    				'posts_per_page'	=> 4,
+    				'post__not_in'		=>array(get_the_ID()),
+    				'orderby'			=>'rand'
+    				)); 
+
+    if ( $loop->have_posts() )
+    {
+
+    	while ( $loop->have_posts() )
+    	{
+    		$loop->the_post();
+
+    		$search	 = Array('{url}','{thumb}','{title}');
+	  		$replace = Array(get_permalink(),get_the_post_thumbnail(),get_the_title());
+    	
+    		$cad .= str_replace($search,$replace, $template_li);
+    	}
+
+    	if ( $cad ) 
+    	{
+		  	$content .= str_replace('{list}', $cad, $template_rel);
+    	}
+
+    }
+   	wp_reset_query();
+
+    return $content;
+}
+
+add_filter( 'the_content', 'dc_related_after_content');*/
--- single.php
+++ single.php
@@ -1,58 +1,108 @@
 <?php 
 
+setup_postdata( $post );
+$thumbID = get_post_thumbnail_id( $post->ID );
+$imgDestacada = wp_get_attachment_image_src( $thumbID, 'large' );
 
-global $category;
 
-$args = array(
-    'taxonomy'	=> "category",
-    'parent'	=> 0,
-    'hide_empty' => 0
-);
-
-$categories = get_categories($args);
 
 get_header(); ?>
-<main class="Main" id="Noticias">
+<main class="Main" id="Single">
     <?php get_template_part( 'navbar'); ?>
 
-    <div class="Content">
-        <div class="Title">
-            <h1>Noticias</h1>
-            <p>Enterate de nuestras últimas noticias e innovaciones</p>
-            <?php var_dump($category) ?>
-        </div>
 
-        <section class="Tags">
-            <p class="Tag-Message">selecciona la categoría que quieres ver</p>
-            <article class="Tag-Content">
-            <a href="#" class="Tag-Item">Todo</a>
-            <?php foreach($categories as $category) : ?>
-
-                <a href="<?php echo get_category_link($category) ?>" class="Tag-Item"><?php echo $category->cat_name ?> </a>
-            <?php endforeach; ?>
-                
-            </article>
-        </section>
+    <div  class="Nosotros-Background" style="background-size:100% auto; height:250px; background-image: url( <?php echo !empty($imgDestacada) ? $imgDestacada[0]: get_template_directory_uri() . '/images/quienes_somos.jpg' ?> )"></div>
 
 
-        <section class="Article-List">
-        <?php while ( have_posts() ) : the_post() ?>
-            <article class="Article-Item" style="background-image: url(<?php echo get_template_directory_uri() . '/images/noti3.png' ?>)">
-                <a href="<?php the_permalink() ?>" class="Article-Link">
 
-                    <div class="Article-Text">
-                        <span class="Article-Text__date"><?php the_time( 'd/m/Y' ) ?></span>
-                        <h4 class="Article-Text__title"><?php the_title() ?></h4>
-                        <span class="Article-Text__paragraph"> <?php the_excerpt() ?> </span>
-                        <span class="Article-Text__link"><?php echo get_the_author()?></span>
-                        <span class="Article-Text__read">Leer más ...</span>
-                    </div>
-                </a>
-            </article>
-            <?php endwhile; ?>
+
+
+
+
+
+
+
+    <section class="Content">
+        
+        <article class="the_content">
+            <h1><?php the_title() ?> </h1>
+            <?php the_content() ?>
+            <div class="the_content_information">
+                <div class="the_author">
+                    Autor: <?php echo get_the_author_firstname() . ' ' . get_the_author_lastname() ?>
+                </div>
+                <div class="the_time">
+                    Publicado: <?php the_time('d/m/Y') ?>
+                </div>
+            </div>
+        </article>
+        
+        <hr>
+
+
+        <?php 
+
+            $terms = get_the_terms( get_the_ID(), 'category');
+            $categ = array();   
+
+            if ( $terms )
+            {
+                foreach ($terms as $term) 
+                {
+                    $categ[] = $term->term_id;
+                }
+            }
+
             
-        </section>
-    </div>
+            if(!empty($categ)) {
+                $loop	= new WP_QUERY(array(
+                    'category__in'		=> $categ,
+                    'posts_per_page'	=> 6,
+                    'post__not_in'		=>array(get_the_ID()),
+                    'orderby'			=>'desc'
+                ));
+            } else {
+                $loop = get_post(array('posts_per_page'	=> 6));
+            }
+        
+        ?>
+
+        <div class="owl-carousel">
+        <?php 
+        if ( $loop->have_posts() ) : 
+            while ( $loop->have_posts() ) :
+                $loop->the_post(); 
+                setup_postdata( $post );
+                $thumbID = get_post_thumbnail_id( $post->ID );
+                $imgDestacada = wp_get_attachment_image_src( $thumbID, 'small' );
+                ?>
+                
+                <div class="item">
+                    <a href="<?php the_permalink() ?>">
+                    <img src="<?php echo empty($imgDestacada) ? $imgDestacada[0] : get_template_directory_uri() . '/images/noti2.png' ?>" alt="" />
+                    <div class="inner">
+                        <span><?php the_title() ?></span>
+                    </div>
+                    </a>
+                </div>
+
+        <?php 
+            endwhile;
+            
+        endif;
+        ?>
+            
+        </div>
+    
+      
+        
+</section>
+
+<hr>
+
+
+
+
 </main>
 
 <?php get_footer(); ?>
