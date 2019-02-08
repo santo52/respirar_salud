0fc212bf39c65edc78b8ae09c91e49bdd49a8e25
Santiago Ruiz Espitia <sanrui@eltiempo.com>
2019-02-08 10:55:27 -0500

    solución de bug bloqueo google maps en iframe

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         <div class="modal-content">
             <h4 class="modal-content-header"></h4>        
             <p class="modal-content-text">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.41969398222!2d-74.07399488523771!3d4.6969162965884035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9ad751764acf%3A0xa678e33330442012!2sCra.+70+%23108-31%2C+Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1547576874012" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
             </p>
         </div>
     </div>
