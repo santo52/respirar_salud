26199e4ade49952c50fa9d0eb8961282ca7d579a
Santiago Ruiz <santiago.ruiz@bewe.io>
2020-05-26 05:54:46 -0500

    config build

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+		typescript: {
+			base: {
+				src: ['ts/**/*.ts'],
+				dest: 'js/functions.js',
+				options: {
+					module: 'amd', //or commonjs
+					target: 'es5', //or es3
+					sourceMap: true,
+					declaration: true
+				}
+			}
+		},
+		sass: {                              // Task 
+			dist: {                            // Target 
+				options: {                       // Target options 
+					style: 'expanded'
+				},
+				files: {                         
+					'css/main.css': 'scss/main.scss',       // 'destination': 'source' 
+				}
+			},
+		},
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'css/main.min.css': ['css/main.css']
+				}
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'js/functions.min.js': ['js/functions.js']
+				}
+			}
+		},
+});
+
+  // Load all plugins.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	
+  
+  // Register all Tasks.
+	grunt.registerTask('default',  ['sass:dist', 'cssmin', 'typescript', 'uglify']);
+};
